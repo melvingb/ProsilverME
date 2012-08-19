12eb46362b5b0a189a8a4d5ebb479215c207afef
nextgen666 <neodarknsf@gmail.com>
2012-08-19 15:41:46 -0600

    fixed visualization in IE and optimize

--- template/prosilverme_script.js
+++ template/prosilverme_script.js
@@ -10,14 +10,14 @@ $(document).ready(function() {
 $(function() {
 
 	// grab the initial top offset of the navigation 
-	var sticky_navigation_offset_top = $('.navbg').offset().top;
+	var navbg_offset_top = $('.navbg').offset().top;
 	
 	// our function that decides weather the navigation bar should have "fixed" css position or not.
-	var sticky_navigation = function(){
+	var navbg = function(){
 		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
 		
 		// if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
-		if (scroll_top > sticky_navigation_offset_top) { 
+		if (scroll_top > navbg_offset_top) { 
 			$('.navbg').css({ 'position': 'fixed', 'top':0, 'left':0 });
 		} else {
 			$('.navbg').css({ 'position': 'relative' }); 
@@ -25,11 +25,11 @@ $(function() {
 	};
 	
 	// run our function on load
-	sticky_navigation();
+	navbg();
 	
 	// and run it again every time you scroll
 	$(window).scroll(function() {
-		 sticky_navigation();
+		 navbg();
 	});
 	
 	// NOT required:
